179238dcb05aed1b229c5b079a5b4da04946f964
Mochachou <frommillersplanet@gmail.com>
2024-07-18 09:40:31 +0900

    deploy: add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,6 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <body>{children}</body>
     </html>
   );
